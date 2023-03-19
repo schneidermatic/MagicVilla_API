d3e3b623dee4eaeab4c12896df402b2976ed19eb
schneidermatic <markus.schneider73@gmail.com>
2023-03-19 14:17:05 +0100

    Chap06 - Repository CleanUp & API Response

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -16,5 +16,6 @@ namespace MagicVilla_VillaAPI.Models
         public string ImageUrl { get; set; }
         public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
